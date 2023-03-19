23c28ab53b1259f605cf8ba38f4a1b35c3633688
redEddie <78861908+redEddie@users.noreply.github.com>
2023-03-19 23:28:51 +0900

    아직 미완인데 쓸만은 한...
    .git파일은 무시할 수 있도록
    토글 되는 아이콘이 이쁘도록
    li 때문인지 엄청 큰 *이 보이는게 거슬림.

--- files/tree_auto_gen copy.py
+++ files/tree_auto_gen copy.py
@@ -7,22 +7,42 @@ def create_index_html(folder_path):
         f.write("<html>\n")
         f.write("<head>\n")
         f.write("<title>{}</title>\n".format(os.path.basename(folder_path)))
+        f.write("<script>\n")
+        f.write('function toggle(element) {\n')
+        f.write('  var ul = element.parentElement.querySelector("ul");\n')
+        f.write('  if (ul) {\n')
+        f.write('    if (ul.style.display === "none") {\n')
+        f.write('      ul.style.display = "block";\n')
+        f.write('      element.innerHTML = "&#9660; " + element.innerHTML.slice(2);\n')
+        f.write('    } else {\n')
+        f.write('      ul.style.display = "none";\n')
+        f.write('      element.innerHTML = "&#9658; " + element.innerHTML.slice(2);\n')
+        f.write('    }\n')
+        f.write('  }\n')
+        f.write('}\n')
+        f.write("</script>\n")
         f.write("</head>\n")
         f.write("<body>\n")
         f.write("<ul>\n")
         for root, dirs, files in os.walk(folder_path):
             level = root.replace(folder_path, '').count(os.sep)
-            indent = "&emsp;" * 4 * (level)  # emsp가 여기있음.
+            indent = "&emsp;" * 4 * (level)
             if level == 0:
                 parent_dir = os.path.dirname(os.path.abspath(folder_path))
                 f.write('<li><a href="{}">../</a></li>\n'.format(parent_dir))
             else:
-                f.write('<li>{}{}</li>\n'.format(indent, os.path.basename(root)))
+                folder_name = os.path.basename(root)
+                f.write(
+                    '<li>{}<span onclick="toggle(this)">&#9658; {}</span>\n'.format(indent, folder_name))
+                f.write('{}  <ul style="display:none">\n'.format(indent))
             subindent = "&emsp;" * 4 * (level + 1)
             for file in files:
                 file_path = os.path.join(root, file)
                 f.write(
                     '<li>{}<a href="{}">{}</a></li>\n'.format(subindent, file_path, file))
+            if level != 0:
+                f.write('{}</ul>\n'.format(indent))
+                f.write('{}</li>\n'.format(indent))
         f.write("</ul>\n")
         f.write("</body>\n")
         f.write("</html>")
